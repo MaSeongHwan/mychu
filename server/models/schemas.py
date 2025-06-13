28f9e1e7366af137a4754b06d3f302a0536e6442
shimhyunji02 <tlaguswl21@gmail.com>
2025-06-14 00:33:32 +0900

    누락된 파일 다시 업로드

---
+++ server/models/schemas.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, String, Float, Integer, Text
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    sha2_hash = Column(String(256), primary_key=True)
+    age_avg = Column(Float)
+    main_channels = Column(Text)
+    use_tms = Column(Float)
+    cnt = Column(Integer)
