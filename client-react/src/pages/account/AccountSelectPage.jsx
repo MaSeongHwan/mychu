d7d5441705b8df15655f57fc9dee083349302b78
sujeong0710 <jhkm2510@gmail.com>
2025-06-26 20:22:42 +0900

    account 페이지 디자인 완성

--- client-react/src/pages/account/AccountSelectPage.jsx
+++ client-react/src/pages/account/AccountSelectPage.jsx
@@ -1,82 +1,64 @@
 import { useNavigate } from 'react-router-dom';
 import './AccountSelectPage.css';
+import logo from '../../assets/images/logos/welllist_backno.png';
+import iconEmail from '../../assets/images/logos/simple_logo.png';
+import iconNaver from '../../assets/images/logos/naver_logo.png';
+import iconKakao from '../../assets/images/logos/kakao_logo.png';
+import bgImage from '../../assets/images/backgrounds/login_image3.png';
 
-/**
- * 계정 선택 페이지 컴포넌트 - HTML/CSS를 React로 구현
- */
-const AccountSelectPage = () => {
+const AccountPage = () => {
   const navigate = useNavigate();
 
-  // 이메일 로그인 처리
-  const handleEmailLogin = () => {
-    navigate('/login');
-  };
-
-  // 네이버 로그인 처리
-  const handleNaverLogin = () => {
-    // 네이버 로그인 로직 구현
-    console.log('네이버 로그인 클릭');
-    // TODO: 네이버 OAuth 로그인 구현
-  };
-
-  // 카카오 로그인 처리
-  const handleKakaoLogin = () => {
-    // 카카오 로그인 로직 구현
-    console.log('카카오 로그인 클릭');
-    // TODO: 카카오 OAuth 로그인 구현
-  };
-
-  // 회원가입 페이지로 이동
-  const handleSignup = () => {
-    navigate('/signup');
-  };
-
   return (
-    <div className="account-select-page">
-      {/* 상단 좌측 로고 */}
-      <header className="account-header">
-        <img 
-          src="/images/welllist_backno.png" 
-          alt="WellList 로고" 
-          className="account-logo"
-          onClick={() => navigate('/')}
-          style={{ cursor: 'pointer' }}
-        />
-      </header>
+    <div
+  className="account-page"
+  style={{
+    backgroundImage: `url(${bgImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    position: 'relative',
+  }}
+>
+      {/* 어두운 오버레이 추가 */}
+      <div className="background-overlay"></div>
 
-      {/* 계정 선택 본문 */}
-      <main className="account-container">
-        <h1 className="account-title">
-          반가워요!<br />
-          계정을 선택해주세요.
-        </h1>
+      {/* 상단 로고 */}
+      <div className="account-logo" onClick={() => navigate('/')}>
+        <img src={logo} alt="WellList 홈으로" />
+      </div>
+
+      {/* 메인 콘텐츠 */}
+      <div className="account-content">
+        <h2 className="account-title">반가워요!<br />계정을 선택해주세요.</h2>
 
         <div className="account-buttons">
-          <button className="account-btn welllist" onClick={handleEmailLogin}>
-            <img src="/images/simple_logo.png" alt="이메일" className="logo-icon" />
-            이메일로 시작하기
+          <button className="account-btn" onClick={() => navigate('/login')}>
+            <div className="btn-icon">
+              <img src={iconEmail} alt="이메일" />
+            </div>
+            <div className="btn-label">이메일로 시작하기</div>
           </button>
-          
-          <button className="account-btn naver" onClick={handleNaverLogin}>
-            <img src="/images/naver_logo.png" alt="네이버" className="logo-icon" />
-            네이버로 시작하기
+          <button className="account-btn">
+            <div className="btn-icon">
+              <img src={iconNaver} alt="네이버" />
+            </div>
+            <div className="btn-label">네이버로 시작하기</div>
           </button>
-          
-          <button className="account-btn kakao" onClick={handleKakaoLogin}>
-            <img src="/images/kakao_logo.png" alt="카카오" className="logo-icon" />
-            카카오로 시작하기
+          <button className="account-btn">
+            <div className="btn-icon">
+              <img src={iconKakao} alt="카카오" />
+            </div>
+            <div className="btn-label">카카오로 시작하기</div>
           </button>
         </div>
 
-        <div className="account-footer">
-          아직 계정이 없으신가요?{' '}
-          <button onClick={handleSignup} className="signup-link">
-            회원가입
-          </button>
-        </div>
-      </main>
+        <p className="signup-text">
+          아직 계정이 없으신가요? <span onClick={() => navigate('/signup')}>회원가입</span>
+        </p>
+      </div>
     </div>
   );
 };
 
-export default AccountSelectPage;
+export default AccountPage;
